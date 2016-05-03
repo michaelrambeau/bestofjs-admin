7a6bfd50749e71389fa62d172303894131eaf58d
Michael Rambeau <mikeair@gmail.com>
2016-05-03 03:15:18 +0000

    Update  model

--- models/Hero.js
+++ models/Hero.js
@@ -10,18 +10,21 @@ var options = {
   defaultSort: '-createdAt',
   track: true, // automatically add `createdAt` and `updatedAt`
   map: {
-    name: 'github.login'
+    name: 'github.login' // 1st field used in list and document views (to avoid displaying '_id' field) 
   }
+  // `autokey` does not work... how to enforce unicity ?
+  // autokey: { path: 'github.login', from: 'github.login', unique: true },
 };
 var Hero = new keystone.List('Hero', options);
 
 Hero.add({
   github: {
     login: { type: Types.Text, required: true, index: true, initial: true },
-    name: { type: Types.Name, required: false, index: true },
-    avatar_url: { type: Types.Url, required: false }
+    name: { type: Types.Text, index: true },
+    avatar_url: { type: Types.Url },
+    followers: { type: Types.Number }
   }
 });
 
-Hero.defaultColumns = 'github.login, github.name, createdAt';
+Hero.defaultColumns = 'github.login, github.name, github.followers, createdAt';
 Hero.register();
