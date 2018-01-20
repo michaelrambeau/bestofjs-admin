99bb831453d4bdd4dda11a8788a131ee5f3388e0
michaelrambeau <mikeair@gmail.com>
2018-01-20 11:01:17 +0900

    Add comments and related fields to Tag model

--- models/Tag.js
+++ models/Tag.js
@@ -13,8 +13,10 @@ Tag.add({
   code: { type: Types.Text, required: true, index: true, initial: true },
   name: { type: Types.Text, required: true, index: true, initial: true },
   description: { type: Types.Textarea, required: false },
+  comments: { type: Types.Textarea, required: false },
+  related: { type: Types.Relationship, ref: 'Tag', many: true },
   aliases: { type: Types.TextArray }
 })
 
-Tag.defaultColumns = 'code, aliases, name, description'
+Tag.defaultColumns = 'code, aliases, name, related, description'
 Tag.register()
