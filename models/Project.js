56e66ea0efa7fd43488cb6dbd8d73c8c50df2a1b
michaelrambeau <mikeair@gmail.com>
2018-07-22 20:11:45 +0900

    Add errorMessage from bundlephobia

--- models/Project.js
+++ models/Project.js
@@ -62,7 +62,8 @@ Project.add({
     dependencyCount: { type: Types.Number },
     gzip: { type: Types.Number },
     size: { type: Types.Number },
-    version: { type: Types.Number }
+    version: { type: Types.Text },
+    errorMessage: { type: Types.Text }
   },
   icon: {
     url: { type: Types.Text },
@@ -88,5 +89,5 @@ Project.schema.methods.toString = function() {
 }
 
 Project.defaultColumns =
-  'name, npm.name, disabled, deprecated, github.pushed_at, tags, github.commit_count, github.contributor_count, github.topics, createdAt'
+  'name, npm.name, bundle.gzip, disabled, deprecated, github.pushed_at, tags, github.commit_count, github.contributor_count, github.topics, createdAt'
 Project.register()
